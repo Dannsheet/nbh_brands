5607b060e490c471f7a7afc9696089d474f1c6a0
Danny <danny@Danny-2.local>
2025-08-30 06:48:50 -0500

    Actualizacion3.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true, // ⚠️ Desactiva el optimizador de Vercel para evitar el error 402
         remotePatterns: [
             {
                 protocol: 'https',
